75d892063df4016d04c072006e063d9779708b96
shauritanga <curtisisaac36@gmail.com>
2025-08-31 21:32:45 +0300

    Add UI components index file to fix Vercel import issues

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+// Re-export all UI components for easier imports
+export * from './button'
+export * from './card'
+export * from './input'
+export * from './label'
+export * from './textarea'
+export * from './badge'
